304fd3d7b28345b3ac041171b04ce3a0f2785f9b
kzhctech <95127984+kzhctech@users.noreply.github.com>
2022-01-08 04:15:24 +0000

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 const mongodb = require("mongodb")
 
-mongodb.connect(process.env.CONNECTIONSTRING, { useUnifiedTopology: true }, function (err, client) {
+mongodb.connect('mongodb+srv://kzhccric:2FQHi2IPGWdllW21@cluster0.v6byg.mongodb.net/kzhcCric?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (err, client) {
   module.exports = client
   const app = require("./app.js")
   app.listen(process.env.PORT || 3000)
