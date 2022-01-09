f0cc57d6ad7d84ab2f7a215f1a11ecde9cd21cd7
kzhctech <95127984+kzhctech@users.noreply.github.com>
2022-01-09 03:06:39 +0000

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,23 @@ dotenv.config()
 const mongodb = require("mongodb")
 
 mongodb.connect('mongodb+srv://kzhccric:2FQHi2IPGWdllW21@cluster0.v6byg.mongodb.net/kzhcCric?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (err, client) {
-  module.exports = client
-  const app = require("./app.js")
+const express = require("express")
+const app = express()
+const client = require("./db.js")
+const db = client.db()
+
+app.get("/", async (req, res) => {
+  try {
+    const news = await db.collection("news").find({ author: "tajbir" }).toArray()
+    if (news.length) {
+      res.json(news)
+    } else {
+      res.json("You do not currently have any dogs in your pets collection.")
+    }
+  } catch (err) {
+    console.log(err)
+    res.json("Try again later.")
+  }
+})
   app.listen(process.env.PORT || 3000)
 })
